bb18762565e3d3fc467ace9fa0637344329f86fa
truemaxdh <truemaxdh@gmail.com>
2020-12-10 16:34:34 +0900

    Update XMasTree.js

--- js/XMasTree.js
+++ js/XMasTree.js
@@ -6,8 +6,6 @@ function _light(cx, cy, r) {
   this.cx = cx;
   this.cy = cy;
   this.r = r;
-  this.dx = dx;
-  this.dy = dy;
   this.style = (Math.random() >= 0.5) ? "fill" : "stroke";
   this.getNewC = function(c) {
     return 80 + Math.random() * 176;
@@ -46,23 +44,21 @@ motionGraphics.xMasTree = function(el) {
   cnv.height = cnv.style.height.replace("px","");
   el.appendChild(cnv);
 
-  var obj = this.balloonsUp;
+  var obj = this.xMasTree;
   obj.ctx = cnv.getContext("2d");
   obj.w = cnv.width;
   obj.h = cnv.height;
   obj.lastTimeStamp = null;
-  obj.baloons = [];
+  obj.lights = [];
   obj.cnt = Math.random() * 10 + 12;
   console.log(obj.w);
   for (var i = 0; i < obj.cnt; i++) {
     var r = Math.min(obj.w, obj.h) / 20 * (1 + Math.random());
-    obj.baloons.push( 
+    obj.lights.push( 
       new _balloon(
         Math.random() * obj.w, 
-        obj.h + r,
-        r,
-        0,
-        - (Math.random() * obj.h / 70 + 1) 
+        Math.random() * obj.h,
+        r
       )
     );
   }
@@ -77,57 +73,33 @@ motionGraphics.xMasTree = function(el) {
       obj.ctx.fillStyle="black";
       obj.ctx.rect(0, 0, obj.w, obj.h);
       obj.ctx.fill();
-
-      for (var i = 0; i < obj.baloons.length; i++) {
-        var b = obj.baloons[i];
+      
+      obj.ctx.beginPath();
+      obj.ctx.fillStyle = "ForestGreen";
+      for (var i = 0; i < 3; i++) {
+        obj.ctx.moveTo(obj.w / 2, obj.h / 20 * (i * 3 + 1) );
+        obj.ctx.lineTo(obj.w / 2 - obj.w / 5, obj.h / 20 * (i * 3 + 2) );
+        obj.ctx.lineTo(obj.w / 2 + obj.w / 5, obj.h / 20 * (i * 3 + 2) );
+      }
+      obj.ctx.fill();
+      
+      for (var i = 0; i < obj.lights.length; i++) {
+        var b = obj.lights[i];
 
         // draw
         obj.ctx.beginPath();
         if (b.style == "fill") {
           obj.ctx.fillStyle = "rgb(" + b.bgr.r + "," + b.bgr.g + "," + b.bgr.b + ")";
           obj.ctx.arc(b.cx, b.cy, b.r, 0, 2 * Math.PI);
-
-          obj.ctx.moveTo(b.cx, b.cy + b.r);
-          obj.ctx.lineTo(b.cx - b.r / 5, b.cy + b.r * 1.2);
-          obj.ctx.lineTo(b.cx + b.r / 5, b.cy + b.r * 1.2);
-          obj.ctx.lineTo(b.cx, b.cy + b.r);
           obj.ctx.fill();
         } else {
           obj.ctx.strokeStyle = "rgb(" + b.bgr.r + "," + b.bgr.g + "," + b.bgr.b + ")";
           obj.ctx.arc(b.cx, b.cy, b.r, 0, 2 * Math.PI);
-
-          obj.ctx.moveTo(b.cx, b.cy + b.r);
-          obj.ctx.lineTo(b.cx - b.r * 0.2, b.cy + b.r * 1.2);
-          obj.ctx.lineTo(b.cx + b.r * 0.2, b.cy + b.r * 1.2);
-          obj.ctx.lineTo(b.cx, b.cy + b.r);
           obj.ctx.stroke();
         }
         
-        obj.ctx.beginPath();
-        obj.ctx.strokeStyle = "rgb(" + b.bgr.r + "," + b.bgr.g + "," + b.bgr.b + ")";
-        obj.ctx.moveTo(b.cx, b.cy + b.r * 1.2);
-        obj.ctx.quadraticCurveTo(b.cx + b.r * 0.15, b.cy + b.r * 1.45, b.cx, b.cy + b.r * 1.7);
-        obj.ctx.quadraticCurveTo(b.cx - b.r * 0.15, b.cy + b.r * 1.95, b.cx, b.cy + b.r * 2.2);
-        obj.ctx.quadraticCurveTo(b.cx + b.r * 0.15, b.cy + b.r * 2.45, b.cx, b.cy + b.r * 2.7);
-        obj.ctx.quadraticCurveTo(b.cx - b.r * 0.15, b.cy + b.r * 2.95, b.cx, b.cy + b.r * 3.2);
-        obj.ctx.stroke();
-        
-        // collision
-        b.cx += b.dx;
-        b.cy += b.dy;
-        
-        if (b.cy < -100) {
-          obj.baloons.splice(i--,1);
-          var r = Math.min(obj.w, obj.h) / 20 * (1 + Math.random());
-          obj.baloons.push( 
-            new _balloon(
-              Math.random() * obj.w, 
-              obj.h + r,
-              r,
-              0,
-              - (Math.random() * obj.h / 70 + 1) 
-            )
-          );
+        if (Math.random() < 0.1) {
+          b.style = (b.style == "fill") ? "stroke" : "fill";
         }
       }
     }
