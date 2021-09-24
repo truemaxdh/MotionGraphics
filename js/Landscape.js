e4e79f503c391f89ce771168b19e8e6f0d7ce3eb
truemaxdh <truemaxdh@gmail.com>
2021-09-24 17:17:47 +0900

    Update Landscape.js

--- js/Landscape.js
+++ js/Landscape.js
@@ -2,7 +2,7 @@ if (typeof motionGraphics === 'undefined' || !motionGraphics) {
   motionGraphics = {};
 }
 
-function _sun(ctx, w, h, cx, cy, r, countdown) {
+function _sun(ctx, w, h, cx, cy, r) {
   this.ctx = ctx;
   this.w = w;
   this.h = h;
@@ -53,6 +53,73 @@ function _sun(ctx, w, h, cx, cy, r, countdown) {
   }
 }
 
+function _tree = function(ctx, w, h, cx, cy) {
+  this.ctx = ctx;
+  this.w = w;
+  this.h = h;
+  this.cx = cx;
+  this.cy = cy;
+  this.rad0 = Math.PI * 270 / 180;
+  this.len0 = 90;
+  this.depth0 = 0;
+  
+  this.drawLeaf = function(x, y, rad, len) {
+    var toX = x + Math.cos(rad) * len;
+    var toY = y + Math.sin(rad) * len;
+    var r = Math.random() * 20 + 5;
+    var c0 = parseInt(Math.random() * 7);
+    var c1 = parseInt(Math.random() * 7 + 9);
+    var c2 = parseInt(Math.random() * 7);
+    ctx.beginPath();
+    ctx.arc(toX, toY, r, 0, 2 * Math.PI);
+    ctx.strokeStyle="#aaa";
+    ctx.lineWidth=2;
+    ctx.fillStyle="#" + c0.toString(16) + c1.toString(16) + c2.toString(16);
+    ctx.stroke();
+    ctx.fill();
+  }
+      
+  this.drawLine = function(x, y, rad, len) {
+    var depth = this.depth;
+    var fromDepth = 40 - 7 * (depth - 1);
+    var toDepth = 40 - 7 * depth;
+    var dDepth = (toDepth - fromDepth) / len;
+    for (var _len = 0; _len <= len; _len++) {
+      var toX = x + Math.cos(rad) * _len;
+      var toY = y + Math.sin(rad) * _len;
+      var width = fromDepth + dDepth * _len;
+      ctx.beginPath();
+      ctx.moveTo(x, y);
+      ctx.lineTo(toX, toY);
+      ctx.lineWidth = width;
+      ctx.strokeStyle="#731100";
+      ctx.stroke();  
+    }
+    
+    this.depth++;
+  }
+  
+  this.drawFrm = function(timeStamp) {
+    if (!obj.lastTimeStamp) obj.lastTimeStamp = timeStamp;
+    if ((timeStamp - obj.lastTimeStamp) > 200) {
+      var depth = this.depth;
+      if (depth < 6) {
+        var goldenRatio = 1 / 1.618;
+        var rnd = Math.floor(Math.random() * 2);
+        var len1 = len * ((rnd == 0) ? goldenRatio : 1);
+        var len2 = len * ((rnd == 0) ? 1 : goldenRatio);
+        var rad1 = rad - Math.PI * 20 / 180;
+        drawLine(toX, toY, rad1, len1, depth1);
+
+        var rad2 = rad + Math.PI * 20 / 180;
+        drawLine(toX, toY, rad2, len2, depth1);
+      } else {
+        drawLeaf(x, y, rad, len, depth);
+      }
+    }
+  }
+}
+
 motionGraphics.landscape = function(el) {
   console.log(el.style);
   
@@ -75,11 +142,16 @@ motionGraphics.landscape = function(el) {
   obj.h = cnv.height;
   obj.lastTimeStamp = null;
   obj.sun = new _sun(obj.ctx, obj.w, obj.h, obj.w * Math.random(), obj.h * Math.random(), 50 + (Math.min(cnv.width, cnv.height) - 50) * Math.random());
+  obj.tree = new _tree(obj.ctx, obj.w, obj.h, obj.w * Math.random(), obj.h * Math.random());
   //obj.mountain = new Mountain();
   //obj.trees = [];
+  obj.frmCount = 0;
   
   obj.drawFrm = function(timeStamp) {
-    obj.sun.drawFrm(timeStamp);
+    if (obj.frmCount < 500)
+      obj.sun.drawFrm(timeStamp);
+    else
+      obj.tree.drawFrm(timeStamp);
     requestAnimationFrame(obj.drawFrm);
   }
   
