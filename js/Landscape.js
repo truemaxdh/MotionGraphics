7748db2092cf70d0a9c6a6f4cb3f96a4613c91b6
truemaxdh <truemaxdh@gmail.com>
2021-10-14 11:01:28 +0900

    Update Landscape.js

--- js/Landscape.js
+++ js/Landscape.js
@@ -9,49 +9,54 @@ function _sun(ctx, w, h, cx, cy, r) {
   this.cx = cx;
   this.cy = cy;
   this.r = r;
-  this.div = Math.floor(12 + 58 * Math.random()) * 2;
+  this.div = getRndInt(12, 88);
   this.bgr = {r:190, g:50, b:50};
   this.lastTimeStamp = null;
-  
-  this.getNewC = function(c) {
-    c += Math.random() * 3 - 1;
-    if (c < 0) c = 0;
-    if (c > 255) c = 255;
-    return c;
-  }
-  
-  this.drawFrm = function(timeStamp) {
-    var obj = this;
-    if (!obj.lastTimeStamp) obj.lastTimeStamp = timeStamp;
-    if ((timeStamp - obj.lastTimeStamp) > 150) {
-      obj.lastTimeStamp = timeStamp;
-      obj.ctx.fillStyle = "DeepSkyBlue";
-      obj.ctx.fillRect(0, 0, obj.w, obj.h);
-      obj.bgr.b = obj.getNewC(obj.bgr.b);
-      obj.bgr.g = obj.getNewC(obj.bgr.g);
-      obj.bgr.r = obj.getNewC(obj.bgr.r);
-
-      obj.ctx.fillStyle = "rgb(" + obj.bgr.r + "," + obj.bgr.g + "," + obj.bgr.b + ")";
-      obj.ctx.beginPath();
     
-      for (var i = 0; i < obj.div; i++) {
-        var rad = i * 2 * Math.PI / obj.div;
-        var tmp_r = obj.r - (Math.random() * 5 - 2);
-        var x = obj.cx + tmp_r * Math.cos(rad);
-        var y = obj.cy + tmp_r * Math.sin(rad);
+  this.drawFrm = function(timeStamp) {
+    if (!this.lastTimeStamp) this.lastTimeStamp = timeStamp;
+    if ((timeStamp - this.lastTimeStamp) > 150) {
+      this.lastTimeStamp = timeStamp;
+      this.ctx.fillStyle = "DeepSkyBlue";
+      this.ctx.fillRect(0, 0, this.w, this.h);
+      this.bgr.b = prune(getRndInt(this.bgr.b - 4, 9));
+      this.bgr.g = prune(getRndInt(this.bgr.g - 4, 9));
+      this.bgr.r = prune(getRndInt(this.bgr.r - 4, 9));
+            
+      this.ctx.fillStyle = getRGBStr(this.bgr.r, this.bgr.g, this.bgr.b);
+      this.ctx.beginPath();
+    
+      for (var i = 0; i < this.div; i++) {
+        var rad = i * 2 * Math.PI / this.div;
+        var tmp_r = getRndInt(this.r - 2, 5);
+        var x = this.cx + tmp_r * Math.cos(rad);
+        var y = this.cy + tmp_r * Math.sin(rad);
 
         // draw
         if (i == 0) {
-          obj.ctx.moveTo(x, y);
+          this.ctx.moveTo(x, y);
         } else {
-          obj.ctx.lineTo(x, y);
+          this.ctx.lineTo(x, y);
         }
       }
-      obj.ctx.fill();
+      this.ctx.fill();
     }
   }
 }
 
+function _land(ctx, w, h) {
+  this.ctx = ctx;
+  this.w = w;
+  this.h = h;
+  this.landHeight = h * 0.7;
+  this.lastTimeStamp = null;
+    
+  this.drawFrm = function(timeStamp) {
+    this.lastTimeStamp = timeStamp;
+    this.ctx.fillStyle = "#c68a12";
+    this.ctx.fillRect(0, this.landHeight, this.w, this.h);
+  }
+}
 function _tree(ctx, w, h, sx, sy) {
   this.ctx = ctx;
   this.w = w;
@@ -59,15 +64,12 @@ function _tree(ctx, w, h, sx, sy) {
   this.sx = sx;
   this.sy = sy;
   this.curParams = [];
-  this.nextParams = [[sx, sy, Math.PI * 270 / 180, 50 + Math.random() * 13, 0]];
+  this.nextParams = [[sx, sy, Math.PI * 270 / 180, getRndInt(50, 13), 0]];
   this.goldenRatio = 1 / 1.618;
-  this.maxDepth = 3 + Math.random() * 3;
-  this.rootWidth = 25 + Math.random() * 6;
-  this.getRndColor = function(base, variant) {
-    return parseInt(base + Math.random() * variant).toString(16);
-  }
-  this.strokeStyle = "#" + this.getRndColor(105, 10) + this.getRndColor(12, 10) + this.getRndColor(0, 10) ;
-  
+  this.maxDepth = getRndInt(3, 3);
+  this.rootWidth = getRndInt(25, 6);
+  this.branchColor = getRndColor(110, 24, 0, 48);
+  console.log(this.branchColor);
   this.drawLeaf = function(param) {
     let cx = param[0];
     let cy = param[1];
@@ -78,15 +80,11 @@ function _tree(ctx, w, h, sx, sy) {
     let toX = cx + Math.cos(rad) * len;
     let toY = cy + Math.sin(rad) * len;
     let r = Math.random() * 10 + 3;
-    let c0 = parseInt(Math.random() * 7);
-    let c1 = parseInt(Math.random() * 7 + 9);
-    let c2 = parseInt(Math.random() * 7);
-    //console.log("#" + c0.toString(16) + c1.toString(16) + c2.toString(16));
     this.ctx.beginPath();
     this.ctx.arc(toX, toY, r, 0, 2 * Math.PI);
     this.ctx.strokeStyle="#aaa";
     this.ctx.lineWidth=2;
-    this.ctx.fillStyle="#" + c0.toString(16) + c1.toString(16) + c2.toString(16);
+    this.ctx.fillStyle=getRndColor(0, 125, 0, 64);
     this.ctx.stroke();
     this.ctx.fill();
   }
@@ -110,22 +108,22 @@ function _tree(ctx, w, h, sx, sy) {
       this.ctx.moveTo(cx, cy);
       this.ctx.lineTo(toX, toY);
       this.ctx.lineWidth = width;
-      this.ctx.strokeStyle = this.strokeStyle;
+      this.ctx.strokeStyle = this.branchColor;
       this.ctx.stroke(); 
     }
 
     let depth1 = depth+1;
     if (depth < 6) {
-      let rnd = Math.floor(Math.random() * 2);
+      let rnd = getRndInt(0, 2);
       let len1 = len * ((rnd == 0) ? this.goldenRatio : 1);
       let len2 = len * ((rnd == 0) ? 1 : this.goldenRatio);
       let rad1 = rad - Math.PI * 15 / 180;
       let rad2 = rad + Math.PI * 15 / 180;
 
-      this.nextParams.push([0 + toX, 0 + toY, 0 + rad1, 0 + len1, 0 + depth1]);
-      this.nextParams.push([0 + toX, 0 + toY, 0 + rad2, 0 + len2, 0 + depth1]);
+      this.nextParams.push([toX, toY, rad1, len1, depth1]);
+      this.nextParams.push([toX, toY, rad2, len2, depth1]);
     } else {
-      this.drawLeaf([0 + toX, 0 + toY, 0 + rad, 0 + len, 0 + depth]);
+      this.drawLeaf([toX, toY, rad, len, depth]);
     }    
   }
   
@@ -164,17 +162,24 @@ motionGraphics.landscape = function(el) {
   obj.w = cnv.width;
   obj.h = cnv.height;
   obj.lastTimeStamp = null;
-  obj.sun = new _sun(obj.ctx, obj.w, obj.h, obj.w * Math.random(), obj.h * 0.3 * Math.random(), 35 + Math.min(cnv.width, cnv.height) * 0.3 * Math.random());
-  obj.tree = new _tree(obj.ctx, obj.w, obj.h, obj.w * Math.random(), obj.h * (0.7 + 0.3 * Math.random()));
+  obj.sun = new _sun(obj.ctx, obj.w, obj.h, getRndInt(0, obj.w), getRndInt(0, obj.h * 0.3), getRndInt(35, Math.min(cnv.width, cnv.height) * 0.3));
+  obj.land = new _land(obj.ctx, obj.w, obj.h);
+  obj.tree = new _tree(obj.ctx, obj.w, obj.h, getRndInt(0, obj.w), getRndInt(obj.h * 0.7, obj.h * 0.3));
+  obj.treeCnt = 1;
+  obj.treeMax = getRndInt(5, 15);
   obj.frmCount = 0;
   
   obj.drawFrm = function(timeStamp) {
-    if (obj.frmCount < 300)
+    if (obj.frmCount < 250) {
       obj.sun.drawFrm(timeStamp);
+      obj.land.drawFrm(timeStamp);
+    } 
     else {
       let isFinished = obj.tree.drawFrm(timeStamp);
-      if (isFinished)
-        obj.tree = new _tree(obj.ctx, obj.w, obj.h, obj.w * Math.random(), obj.h * (0.7 + 0.3 * Math.random()));
+      if (isFinished && obj.treeCnt < obj.treeMax) {
+        obj.tree = new _tree(obj.ctx, obj.w, obj.h, getRndInt(0, obj.w), getRndInt(obj.h * 0.7, obj.h * 0.3));
+        obj.treeCnt++;
+      }        
     }
     obj.frmCount++;
     requestAnimationFrame(obj.drawFrm);
